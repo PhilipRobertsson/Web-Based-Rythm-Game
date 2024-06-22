2568e6db8da8d2168258ead05b74be1e8c38a9a2
PhilipRobertsson <phiro138@student.liu.se>
2024-06-22 15:07:57 +0200

    Menu windows added display of them needs to be fixed

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from "react";
+import { useState } from "react";
 import React from 'react';
 
 function App() {
-  
-  // Keyboard events
+
+  const [menuOut, setMenuOut] = useState(false)
+  const [infoOut, setInfoOut] = useState(false)
+
+
   useEffect(() => {
+    // Keyboard events
     function handleKeyDown(e) {
       if(e.code == "Space"){
         console.log(e.code);
         console.log("Judge player in judgment zone");
       }
       if(e.code == "Escape"){
-        console.log(e.code);
-        console.log("Open menu");
+        slideOutMenu("menu");
       }
       if(e.code =="KeyI"){
-        console.log(e.code);
-        console.log("Open instructions");
+        slideOutMenu("info");
+      }
+    }
+    // Slide Out Menus
+    function slideOutMenu(option){
+      if(option == "menu"){
+        if(menuOut == false){
+          setMenuOut(true);
+          console.log("Menu Out")
+          document.getElementById("Menu-window").style.display="flex";
+        }else{
+          setMenuOut(false);
+          console.log("Menu In")
+          document.getElementById("Menu-window").style.display="none";
+        }
+      }
+      if(option == "info"){
+        if(infoOut == false){
+          setInfoOut(true);
+          console.log("Info Out")
+          document.getElementById("Instruction-window").style.display="flex";
+        }else{
+          setInfoOut(false);
+          console.log("Info In")
+          document.getElementById("Instruction-window").style.display="none";
+        }
       }
     }
 
@@ -26,7 +54,7 @@ function App() {
     return function cleanup() {
       document.removeEventListener('keydown', handleKeyDown);
     }
-  }, []);
+  }, [menuOut, infoOut]);
 
   // App content
   return (
@@ -37,7 +65,24 @@ function App() {
         <button className="Info-button">INFO</button>
       </header>
 
+      <div className="Menus-container">
+        <div id="Menu-window">
+            <h3>Menu</h3>
+        </div>
+
+        <div id="Instruction-window">
+            <h3>Instructions</h3>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+      </div>
+        
       <div className="App-content">
+
         <div className="Score-Window">
           <p>score</p>
         </div>
@@ -46,19 +91,10 @@ function App() {
             <div className="Judgement-zone">
 
             </div>
-            <div className="Missed-box">
+            {/*<div className="Missed-box">
 
-            </div>
-        </div>
+            </div>*/}
 
-        <div className="Instruction-window">
-          <h3>Instructions</h3>
-          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-               in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-               occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
         </div>
 
       </div>
